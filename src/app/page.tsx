f332849b9dd7466218aa758d6b017fd68bda3ab4
Alan Duarte <133732698+dev-alanduarte@users.noreply.github.com>
2023-07-05 17:15:09 -0300

    feat: add next providers/auth.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
-import Image from 'next/image'
+'use client'
+import { signIn, signOut, useSession } from 'next-auth/react'
+
+
 
 export default function Home() {
+  const {data} = useSession();
   return (
-   <h1>Home page</h1>
-)
+    <div>
+      
+    </div>
+);
 }
