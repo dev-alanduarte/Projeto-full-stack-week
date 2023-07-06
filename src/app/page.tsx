e2beb5476fa88fa5e18e66c51a28db7d392f62a7
Alan Duarte <133732698+dev-alanduarte@users.noreply.github.com>
2023-07-06 18:00:47 -0300

    feat: add commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,16 @@
 'use client'
 
+import React from "react";
+import RecommendTrips from "./components/RecommendTrips";
 import QuickSearch from "./components/QuickSearch";
 import TripSearch from "./components/TripSearch";
 
 export default function Home() {
   return (
     <div>
-      <TripSearch/>
-      <QuickSearch/>
+      <TripSearch />
+      <QuickSearch />
+      <RecommendTrips />
     </div>
-);
+  );
 }
