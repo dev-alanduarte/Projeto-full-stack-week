2eaf984bdbf303d730b45ce665dbe837523ad598
Alan Duarte <133732698+dev-alanduarte@users.noreply.github.com>
2023-07-07 14:35:39 -0300

    feat: add button componets e style border reservation

---
+++ src/app/trips/[tripId]/components/TripReservation.tsx
@@ -0,0 +1,35 @@
+'use client'
+
+import DatePicker from '@/components/DatePicker'
+import React from 'react'
+import Input from '@/components/Input'
+import { Trip } from '@prisma/client'
+import Button from '@/components/Button'
+
+interface TripReservationProps {
+    trip: Trip;
+}
+
+const TripReservation = ({trip}: TripReservationProps) => {
+  return (
+    <div className="flex flex-col px-5">
+        <div className='flex gap-4'>
+            <DatePicker placeholderText='Data de Inicio' onChange={() => {}} className='w-full'/>
+            <DatePicker placeholderText='Data Final' onChange={() => {}} className='w-full'/>
+        </div>
+
+        <Input placeholder={`Número de Hóspedes (max:${trip.maxguests})`}  className='mt-4'/>
+
+        <div className="flex justify-between mt-3">
+            <p className='font-medium text-sm text-primaryDarker'>Total:</p>
+            <p className='font-medium text-sm text-primaryDarker'>R$2500</p>
+        </div>
+
+          <div className='pb-10 border-b border-grayLighter w-full'>
+        <Button className='mt-3 w-full'>Reservar agora</Button>
+        </div>
+    </div>
+  )
+}
+
+export default TripReservation
